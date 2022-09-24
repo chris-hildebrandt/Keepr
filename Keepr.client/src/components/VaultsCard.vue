2bf0f7f2e9ac9c7eed9d34dcfcf66521c359eab9
chris-hildebrandt <ch.hild15@gmail.com>
2022-09-24 14:05:21 -0600

    keepscard cloned for vaults

---
+++ Keepr.client/src/components/VaultsCard.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <div v-if="vault.id" class="image rounded elevation-3">
+      <img class="img-fluid" :src="vault.img" alt="">
+      <h2>
+        {{vault.name}}
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from "vue";
+
+export default {
+  setup() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.image { 
+   position: relative; 
+   width: 100%;
+}
+
+h2 { 
+   position: absolute; 
+   top: 5px; 
+   left: 10px; 
+   width: 90%; 
+   background-color: rgb(0, 0, 0, .5);
+   text-shadow: 0px 2px 2px black;
+}
+</style>
