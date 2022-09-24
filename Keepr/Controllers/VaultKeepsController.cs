cb5b7dd141cd5d75ab00cfb465003d6fd1636936
chris-hildebrandt <ch.hild15@gmail.com>
2022-09-24 13:30:42 -0600

    vaultKeepsController up

--- Keepr/Controllers/VaultKeepsController.cs
+++ Keepr/Controllers/VaultKeepsController.cs
@@ -1,15 +1,53 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using Keepr.Models;
+using Keepr.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Keepr.Controllers
 {
-    [ApiController]
+  [ApiController]
     [Route("api/[controller]")]
     public class VaultKeepsController : ControllerBase
     {
-        
+        private readonly VaultKeepsService _vaultKeepsService;
+
+    public VaultKeepsController(VaultKeepsService vaultKeepsService)
+    {
+      _vaultKeepsService = vaultKeepsService;
     }
+// I never need to get vaultkeeps directly, i just use them to join vaults to keeps, so my only cruds are create and delete
+    [HttpPost]
+    [Authorize]
+    // the body needs to include the vaultId and the keepId
+    public async Task<ActionResult<VaultKeep>> AddKeepToVault([FromBody] VaultKeep vaultKeepData){
+        try
+      {
+        Account user = await HttpContext.GetUserInfoAsync<Account>();
+        vaultKeepData.CreatorId = user.Id;
+        VaultKeep vaultKeep = _vaultKeepsService.AddKeepToVault(vaultKeepData);
+        return Ok(vaultKeep);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+    [HttpDelete("{id}")]
+    [Authorize]
+    public async Task<ActionResult<string>> RemoveKeepFromVault(int id){
+        try
+      {
+        Account user = await HttpContext.GetUserInfoAsync<Account>();
+        string message = _vaultKeepsService.RemoveKeepFromVault(id, user);
+        return Ok(message);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+  }
 }
--- Keepr/Models/VaultKeep.cs
+++ Keepr/Models/VaultKeep.cs
@@ -1,12 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace Keepr.Models
 {
-    public class VaultKeep
+  public class VaultKeep
     {
-        
+        public int Id { get; set; }
+        public string CreatorId { get; set; }
+        public int VaultId { get; set; }
+        public int KeepId { get; set; }
     }
 }
