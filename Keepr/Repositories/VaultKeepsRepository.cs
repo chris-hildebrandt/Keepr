a73670ad792ab92f60cec1663d0f9431a3266724
chris-hildebrandt <ch.hild15@gmail.com>
2022-09-24 21:53:58 -0600

    backend soft completed

--- Keepr/Repositories/VaultKeepsRepository.cs
+++ Keepr/Repositories/VaultKeepsRepository.cs
@@ -1,12 +1,50 @@
-using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
-using System.Threading.Tasks;
+using Dapper;
+using Keepr.Models;
 
 namespace Keepr.Repositories
 {
-    public class VaultKeepsRepository
+  public class VaultKeepsRepository
+  {
+    private readonly IDbConnection _db;
+
+    public VaultKeepsRepository(IDbConnection db)
     {
-        
+      _db = db;
     }
+
+    internal List<VaultKeep> GetAll()
+    {
+      string sql = "SELECT * FROM vaultKeeps;";
+      List<VaultKeep> vaultKeeps = _db.Query<VaultKeep>(sql).ToList();
+      return vaultKeeps;
+    }
+
+    internal VaultKeep AddKeepToVault(VaultKeep vaultKeepData)
+    {
+      string sql = @"INSERT INTO vaultKeeps
+      (creatorId, vaultId, keepId)
+      VALUES
+      (@creatorId, @vaultId, @keepId);
+      SELECT LAST_INSERT_ID();";
+      int id = _db.ExecuteScalar<int>(sql, vaultKeepData);
+      vaultKeepData.Id = id;
+      return vaultKeepData;
+    }
+
+    internal VaultKeep GetById(int id)
+    {
+      string sql = "SELECT * FROM vaultKeeps WHERE id = @id;";
+      VaultKeep vaultKeep = _db.Query<VaultKeep>(sql, new {id}).FirstOrDefault();
+      return vaultKeep;
+    }
+
+    internal void RemoveKeepFromVault(int id)
+    {
+      string sql = "DELETE FROM vaultKeeps WHERE id = @id;";
+      _db.Execute(sql, new {id});
+    }
+  }
 }
