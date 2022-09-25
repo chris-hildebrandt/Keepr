05c9dbb47458e5f63b92ef66669c640f402a1736
chris-hildebrandt <ch.hild15@gmail.com>
2022-09-25 01:59:16 -0600

    backend profile functionality created

--- Keepr/Controllers/AccountController.cs
+++ Keepr/Controllers/AccountController.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using Keepr.Models;
 using Keepr.Services;
@@ -9,66 +8,30 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Keepr.Controllers
 {
-    [ApiController]
-    [Route("[controller]")]
-    public class AccountController : ControllerBase
-    {
-        private readonly AccountService _accountService;
-        private readonly KeepsService _keepsService;
-        private readonly VaultsService _vaultsService;
+  [ApiController]
+  [Route("[controller]")]
+  public class AccountController : ControllerBase
+  {
+    private readonly AccountService _accountService;
 
-    public AccountController(AccountService accountService, KeepsService keepsService, VaultsService vaultsService)
+    public AccountController(AccountService accountService)
     {
       _accountService = accountService;
-      _keepsService = keepsService;
-      _vaultsService = vaultsService;
     }
 
     [HttpGet]
-        [Authorize]
-        public async Task<ActionResult<Account>> Get()
-        {
-            try
-            {
-                Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
-                return Ok(_accountService.GetOrCreateProfile(userInfo));
-            }
-            catch (Exception e)
-            {
-                return BadRequest(e.Message);
-            }
-        }
-
-            // STUB get all keeps created by a profile id
-    [HttpGet("{userId}/keeps")]
-    public ActionResult<List<Keep>> GetAllProfileKeeps(string userId)
+    [Authorize]
+    public async Task<ActionResult<Account>> Get()
     {
       try
       {
-        List<Keep> keeps = _keepsService.GetAllProfileKeeps(userId);
-        return Ok(keeps);
+        Account userInfo = await HttpContext.GetUserInfoAsync<Account>();
+        return Ok(_accountService.GetOrCreateProfile(userInfo));
       }
       catch (Exception e)
       {
         return BadRequest(e.Message);
       }
     }
-
-        // STUB get all Vaults created by a profile id
-    [HttpGet("{userId}/vaults")]
-    public async Task<ActionResult<List<Vault>>> GetAllProfileVaults(string userId)
-    {
-      try
-      {
-        Account user = await HttpContext.GetUserInfoAsync<Account>();
-        List<Vault> vaults = _vaultsService.GetAllProfileVaults(userId, user.Id);
-        return Ok(vaults);
-      }
-      catch (Exception e)
-      {
-        return BadRequest(e.Message);
-      }
-    }
-    }
-
+  }
 }
---
+++ Keepr/Controllers/ProfilesController.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using CodeWorks.Auth0Provider;
+using Keepr.Models;
+using Keepr.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Keepr.Controllers
+{
+  [ApiController]
+    [Route("api/[controller]")]
+    public class ProfilesController : ControllerBase
+    {
+        private readonly AccountService _accountService;
+        private readonly KeepsService _keepsService;
+        private readonly VaultsService _vaultsService;
+
+    public ProfilesController(AccountService accountService, KeepsService keepsService, VaultsService vaultsService)
+    {
+      _accountService = accountService;
+      _keepsService = keepsService;
+      _vaultsService = vaultsService;
+    }
+    [HttpGet("{id}")]
+    public ActionResult<Profile> GetById(string id){
+        try
+            {
+                Profile profile = _accountService.GetProfile(id);
+                return Ok(profile);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+    }
+                // STUB get all keeps created by a profile id
+    [HttpGet("{userId}/keeps")]
+    public ActionResult<List<Keep>> GetAllProfileKeeps(string userId)
+    {
+      try
+      {
+        List<Keep> keeps = _keepsService.GetAllProfileKeeps(userId);
+        return Ok(keeps);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+
+        // STUB get all Vaults created by a profile id
+    [HttpGet("{userId}/vaults")]
+    public async Task<ActionResult<List<Vault>>> GetAllProfileVaults(string userId)
+    {
+      try
+      {
+        Account user = await HttpContext.GetUserInfoAsync<Account>();
+        List<Vault> vaults = _vaultsService.GetAllProfileVaults(userId, user.Id);
+        return Ok(vaults);
+      }
+      catch (Exception e)
+      {
+        return BadRequest(e.Message);
+      }
+    }
+  }
+}
