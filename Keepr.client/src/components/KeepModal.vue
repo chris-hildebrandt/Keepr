b8750cd48e8e5874c8c4fe988eed7abdbe4e35c6
chris-hildebrandt <ch.hild15@gmail.com>
2022-09-28 22:46:02 -0600

    keep modal dropdown started

--- Keepr.client/src/components/KeepModal.vue
+++ Keepr.client/src/components/KeepModal.vue
@@ -11,9 +11,19 @@
 
               <div class="col-12 col-md-6">
                 <div class="row mt-0 me-3 ">
+<!-- TODO ask about why dropdown and data-target attributes aren't working -->
+                  <div class="col-3 align-self-end dropdown">
+                    <h5 class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><i class="mdi mdi-plus-thick"></i> <b> to Vault</b></h5>
+                    <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                      <a class="dropdown-item" href="#">Action</a>
+                      <a class="dropdown-item" href="#">Another action</a>
+                      <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                  </div>
+
                   <h5 class="col-2 offset-2 align-self-end"><i class="mdi mdi-eye"></i> {{keep.views}}</h5>
                   <h5 class="col-2 align-self-end"><i class="mdi mdi-lock"></i> {{keep.kept}}</h5>
-                  <span id="modal-close" type="button" class="mdi mdi-close mdi-36px col-1 offset-5 mt-0 pt-0"
+                  <span id="modal-close" type="button" class="mdi mdi-close mdi-36px col-1 offset-2 mt-0 pt-0"
                     data-bs-dismiss="modal"></span>
                 </div>
                 <h1 class="modal-title text-center" id="keep-modalLabel">{{keep.name}}</h1>
@@ -75,12 +85,26 @@ export default {
     position: absolute;
     bottom: 3%;
     left: 50%;
+    &:hover {
+    transform: scale(1.01);
+    transition: ease-in-out;
+    filter: hue-rotate(60deg);
+  }
   }
 
   .trash{
+    color: dodgerblue;
     position: absolute;
     bottom: 2%;
     right: 1%;
+      &:hover {
+      transform: scale(1.01);
+      transition: ease-in-out;
+      filter: hue-rotate(125deg);
+  }
   }
 
+h5{
+  color: dodgerblue;
+}
 </style>
