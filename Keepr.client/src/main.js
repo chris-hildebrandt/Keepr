9ce26b63e96f5384b4974079fa5c9a6e0e4daa94
chris-hildebrandt <ch.hild15@gmail.com>
2022-10-06 11:22:14 -0600

    added firebase blah

--- Keepr.client/src/main.js
+++ Keepr.client/src/main.js
@@ -6,6 +6,28 @@ import App from './App.vue'
 import { registerGlobalComponents } from './registerGlobalComponents'
 import { router } from './router'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBU6XoizmM2eMtQgyxOQgIykCaYom7ssC4",
+  authDomain: "keepr-75c5f.firebaseapp.com",
+  projectId: "keepr-75c5f",
+  storageBucket: "keepr-75c5f.appspot.com",
+  messagingSenderId: "161120860291",
+  appId: "1:161120860291:web:87c975c9403a3cbaf9c3b6",
+  measurementId: "G-LRZGRMDT2G"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 const root = createApp(App)
 registerGlobalComponents(root)
 
